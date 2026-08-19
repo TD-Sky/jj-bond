b15134916dd8987568f4af447edb35900e966ad9
TD-Sky <three-dim-sky@foxmail.com>
2026-08-19 22:26:33 +0800

    fix: optional VERGEN_GIT_SHA

--- build.rs
+++ build.rs
@@ -1,22 +1,12 @@
-use std::{env, error::Error};
+use std::error::Error;
 
 use vergen_gix::{Build, Emitter, Gix};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut v = Emitter::default();
-
-    v.add_instructions(&Build::builder().build_date(true).build())?;
-
-    match env::var_os("JB_NO_GIT") {
-        Some(_) => {
-            println!("cargo::rustc-env=VERGEN_GIT_SHA=no-git");
-        }
-        None => {
-            v.add_instructions(&Gix::builder().sha(true).build())?;
-        }
-    }
-
-    v.emit()?;
+    Emitter::default()
+        .add_instructions(&Build::builder().build_date(true).build())?
+        .add_instructions(&Gix::builder().sha(true).build())?
+        .emit()?;
 
     Ok(())
 }
